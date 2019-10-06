75f22f79c8f03c221bfe7515c3929a111ee3c0f5
root <root@ip-172-31-5-222.ap-south-1.compute.internal>
2019-10-06 11:14:48 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi Jesu-Devops Engineer,Bangalore.Next week is your target to complete all the learnings...!!!
         </p>
         <a
           className="App-link"
